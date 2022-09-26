8d133a36fe8cd4b637568f5550e86757e25838e0
jaejun1130 <wjswowns1234@gmail.com>
2022-09-26 21:18:35 +0900

    init

---
+++ src/test/java/jaejun/gop/GopApplicationTests.java
@@ -0,0 +1,13 @@
+package jaejun.gop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
